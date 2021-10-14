3190bc100c1c7dd77239a8cae0020d7577f7c3cd
Blifers <pwilya3@gmail.com>
2021-10-14 17:40:02 +0700

    Алгоритм чтения файлов

--- MobChecker/Forms/MainMenu.cs
+++ MobChecker/Forms/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,28 @@ namespace MobChecker
         private void UploadButton_Click(object sender, EventArgs e)
         {
             ofdUpload.ShowDialog();
-
-            foreach (string file in ofdUpload.FileNames)
+            int t = 0;
+            /*foreach (string file in ofdUpload.FileNames)
             {
                 MessageBox.Show(file);
                 //Filename Contains full path to the file with the extension
+            }*/
+
+            //Can't do this cause the multiselect is on... but for test
+            using(var reader = new StreamReader(ofdUpload.FileName))
+            {
+                List<string> Values = new List<string>();
+                
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    if (t == 0)
+                        MessageBox.Show(values[2]);
+                    t++;
+                }
             }
+
         }
     }
 }
