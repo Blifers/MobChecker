4da82c4e6e078f95cdb6f20ee1460d663d979cf4
Ilya <Ilya@ILYA>
2021-10-14 08:06:11 +0700

    Добавьте файлы проекта.

---
+++ MobChecker/Classes/PlayerHunt.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MobChecker.Classes
+{
+    class PlayerHunt
+    {
+        private long UserID;
+        private string NickName;
+        private int Total;
+        private int Hunt;
+        private int Purchase;
+        
+        //Монстры 1-5 тира
+        private int Tier1Mob;
+        private int Tier2Mob;
+        private int Tier3Mob;
+        private int Tier4Mob;
+        private int Tier5Mob;
+
+        //Покупки 1-5 тира
+        private int Tier1Purchase;
+        private int Tier2Purchase;
+        private int Tier3Purchase;
+        private int Tier4Purchase;  
+        private int Tier5Purchase;
+
+        private int Points;
+        private string GoalPercentage; // ?
+
+
+        public PlayerHunt(string [] playerInfo)
+        {
+            FillData(playerInfo);
+        }
+
+        public PlayerHunt()
+        {
+
+        }
+
+        public void FillData (string [] playerInfo)
+        {
+            FillData(playerInfo);
+        }
+
+        private void FillClass(string[] playerInfo)
+        {
+            try
+            {
+                UserID = Convert.ToInt64(playerInfo[0]);
+                NickName = playerInfo[1];
+                Total = Convert.ToInt32(playerInfo[2]);
+                Hunt = Convert.ToInt32(playerInfo[3]);
+                Purchase = Convert.ToInt32(playerInfo[4]);
+                Tier1Mob = Convert.ToInt32(playerInfo[5]);
+                Tier2Mob = Convert.ToInt32(playerInfo[6]);
+                Tier3Mob = Convert.ToInt32(playerInfo[7]);
+                Tier4Mob = Convert.ToInt32(playerInfo[8]);
+                Tier5Mob = Convert.ToInt32(playerInfo[9]);
+                Tier1Purchase = Convert.ToInt32(playerInfo[10]);
+                Tier2Purchase = Convert.ToInt32(playerInfo[11]);
+                Tier3Purchase = Convert.ToInt32(playerInfo[12]);
+                Tier4Purchase = Convert.ToInt32(playerInfo[13]);
+                Tier5Purchase = Convert.ToInt32(playerInfo[14]);
+                Points = Convert.ToInt32(playerInfo[15]);
+                GoalPercentage = playerInfo[16];
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public void AddData(string [] playerInfo)
+        {
+            long userID = Convert.ToInt64(playerInfo[0]);
+
+            if (UserID != userID)
+                return;
+            else
+                AddHuntStats(playerInfo);
+        }
+
+        private void AddHuntStats(string [] playerInfo)
+        {
+            Total += Convert.ToInt32(playerInfo[2]);
+            Hunt += Convert.ToInt32(playerInfo[3]);
+            Purchase += Convert.ToInt32(playerInfo[4]);
+            Tier1Mob += Convert.ToInt32(playerInfo[5]);
+            Tier2Mob += Convert.ToInt32(playerInfo[6]);
+            Tier3Mob += Convert.ToInt32(playerInfo[7]);
+            Tier4Mob += Convert.ToInt32(playerInfo[8]);
+            Tier5Mob += Convert.ToInt32(playerInfo[9]);
+            Tier1Purchase += Convert.ToInt32(playerInfo[10]);
+            Tier2Purchase += Convert.ToInt32(playerInfo[11]);
+            Tier3Purchase += Convert.ToInt32(playerInfo[12]);
+            Tier4Purchase += Convert.ToInt32(playerInfo[13]);
+            Tier5Purchase += Convert.ToInt32(playerInfo[14]);
+            Points += Convert.ToInt32(playerInfo[15]);
+        }
+
+        public string FailureCheck(int days)
+        {
+            return "";
+        }
+    }
+}
---
+++ MobChecker/Forms/MainMenu.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace MobChecker
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdUpload = new System.Windows.Forms.OpenFileDialog();
+            this.UploadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ofdUpload
+            // 
+            this.ofdUpload.Multiselect = true;
+            // 
+            // UploadButton
+            // 
+            this.UploadButton.Location = new System.Drawing.Point(682, 403);
+            this.UploadButton.Name = "UploadButton";
+            this.UploadButton.Size = new System.Drawing.Size(106, 35);
+            this.UploadButton.TabIndex = 0;
+            this.UploadButton.Text = "Загрузить";
+            this.UploadButton.UseVisualStyleBackColor = true;
+            this.UploadButton.Click += new System.EventHandler(this.UploadButton_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UploadButton);
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdUpload;
+        private System.Windows.Forms.Button UploadButton;
+    }
+}
+
